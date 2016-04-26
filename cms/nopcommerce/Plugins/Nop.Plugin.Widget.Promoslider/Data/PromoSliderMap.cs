0c820ee435cf91477a5ca1f06d1e12d4786e2aeb
Andrey Mironoff <mironoff@madlabs.lv>
2016-04-26 20:54:23 +0300

    Add initial training solution

---
+++ cms/nopcommerce/Plugins/Nop.Plugin.Widget.Promoslider/Data/PromoSliderMap.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nop.Plugin.Widget.PromoSliderPlugin.Domain;
+
+namespace Nop.Plugin.Widget.PromoSliderPlugin.Data
+{
+    public class PromoSliderMap : EntityTypeConfiguration<PromoSlider>
+    {
+        public PromoSliderMap()
+        {
+            ToTable("PromoSlider_PromoSliders");
+            HasKey(m => m.PromoSliderId);
+
+            Property(m => m.PromoSliderName);
+            Property(m => m.ZoneName);
+            Property(m => m.Interval);
+            Property(m => m.KeyBoard);
+            Property(m => m.PauseOnHover);
+            Property(m => m.Wrap);
+        }
+    }
+}
