9d4cb14626916399db30247822533ce4329d5094
Andrey Mironoff <mironoff@madlabs.lv>
2016-06-13 17:40:41 +0300

    Training code for Building Secure Web APIs with ASP.NET Core
    https://channel9.msdn.com/Events/dotnetConf/2016/Building-Secure-Web-APIs-with-ASPNET-Core

---
+++ dotNetConf16/SecureApisWithAspNetCore/FirstCoreApi/src/FirstCoreApi/Controllers/ToDosController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstCoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class ToDosController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<Value> Get()
+        {
+            return new Value[] { new Value() {Id = 1, Text = "Some text"}, new Value() { Id = 2, Text="Some other text"} };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id:int}")]
+        public string Get(int id)
+        {
+            return $"value {id}";
+        }
+
+        // POST api/values
+        [HttpPost]
+        [Produces("application/json", Type = typeof(Value))]
+        [Consumes("application/json")]
+        public ActionResult Post([FromBody]Value incomingValue)
+        {
+            return CreatedAtAction("Get", "ToDos", new {id = incomingValue.Id}, incomingValue);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+
+    public class Value
+    {
+        public int Id { get; set; }
+
+        public string Text { get; set; }
+    }
+}
